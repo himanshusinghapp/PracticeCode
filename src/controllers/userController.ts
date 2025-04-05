17809d691d096cc744d0baf774fc5f2d0867822c
himanshusinghapp <himanshu.singh2@appinventiv.com>
2025-04-05 15:02:26 +0530

    add constants file to put status code or messages

--- src/controllers/userController.ts
+++ src/controllers/userController.ts
@@ -2,16 +2,16 @@ import { Request,Response } from "express";
 import bcrypt from "bcryptjs";
 import User,{IUser} from "../models/User";
 import { userValidationSchema } from "../validation/userValidation";
-
+import { MESSAGES,STATUS_CODES } from "../constants";
 
 export const userRegister = async(req:Request,res:Response)=>{
     try {
         const { error } = userValidationSchema.validate(req.body);
-        if (error) return res.status(400).json({ message: error.details[0].message });
+        if (error) return res.status(STATUS_CODES.BAD_REQUEST).json({ message: error.details[0].message });
     
         const { name, age, email, password, phoneNumber } = req.body;
         const existingUser = await User.findOne({ email });
-        if (existingUser) return res.status(400).json({ message: "Email already registered" });
+        if (existingUser) return res.status(400).json({ message: MESSAGES.USER_EXISTS });
 
         const salt = await bcrypt.genSalt(10);
         const hashedPassword = await bcrypt.hash(password, salt);
@@ -25,9 +25,9 @@ export const userRegister = async(req:Request,res:Response)=>{
         });
     
         await newUser.save();
-        res.status(201).json({ message: "User registered successfully" });
+        res.status(STATUS_CODES.CREATED).json({ message: MESSAGES.USER_CREATED });
     } catch (error) {
-        res.status(500).json({ message: "Server Error", error });
+        res.status(STATUS_CODES.SERVER_ERROR).json({ message:MESSAGES.SERVER_ERROR, error });
     }
 };
 //login
@@ -37,16 +37,16 @@ export const loginUser = async(req: Request, res: Response)=>{
         const {email, password}=req.body;
         const user = await User.findOne({email});
         if(!user){
-            return res.status(400).json({message:"Login failed"});
+            return res.status(STATUS_CODES.BAD_REQUEST).json({message:MESSAGES.INVALID_CREDENTIALS});
         }
         const isPassMatch = await bcrypt.compare(password,user.password);
         if(!isPassMatch){
-            return res.status(400).json({message:"Login failed"});
+            return res.status(STATUS_CODES.BAD_REQUEST).json({message:MESSAGES.INVALID_CREDENTIALS});
         }
-        res.status(200).json({message:"Login successfully"});
+        res.status(STATUS_CODES.SUCCESS).json({message:MESSAGES.USER_LOGIN});
     }
     catch(err){
-        res.status(500).json({message:"internal server error",err});
+        res.status(STATUS_CODES.SERVER_ERROR).json({message:MESSAGES.SERVER_ERROR,err});
     }
 };
 
@@ -55,8 +55,8 @@ export const loginUser = async(req: Request, res: Response)=>{
 export const getUsers = async (req: Request, res: Response) => {
     try {
         const users = await User.find();
-        res.status(200).json(users);
+        res.status(STATUS_CODES.SUCCESS).json(users);
     } catch (error) {
-        res.status(500).json({ message: "Server Error", error });
+        res.status(STATUS_CODES.SERVER_ERROR).json({ message: MESSAGES.SERVER_ERROR, error });
     }
 };
